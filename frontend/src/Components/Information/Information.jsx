f7b0baa36ddefea37c75c72cc19d32ede1b9a551
Harshit Bareja <hbareja.07@gmail.com>
2024-08-31 11:53:31 +0530

    created information page

--- frontend/src/Components/Information/Information.jsx
+++ frontend/src/Components/Information/Information.jsx
@@ -3,7 +3,7 @@ import './information.css'
 import logo from '../../Assets/logo.png'
 const Information = () => {
   return (
-    <section id='info' className='info-page'>
+    <section id='info' className='main-page'>
       <div className="user-info-container">
         <div className="user-profile-icon">
           <img src={logo} alt="ACM"/>
@@ -11,6 +11,7 @@ const Information = () => {
         <div className="user-info">
           <span>My Profile</span>
           <input placeholder='Name...'/>
+          <input placeholder='Roll Number...'/>
           <input placeholder='Position...'/>
           <button>Submit</button>
         </div>
