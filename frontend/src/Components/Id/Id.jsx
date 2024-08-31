4c0d6963eb63a4c411756f938a437143d6131f85
Harshit Bareja <hbareja.07@gmail.com>
2024-08-31 15:46:55 +0530

    Id card generated

--- frontend/src/Components/Id/Id.jsx
+++ frontend/src/Components/Id/Id.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import logo from '../../Assets/logo.png'
-import { useSelector } from 'react-redux'
-import './Id.css'
+import React from 'react';
+import logo from '../../Assets/logo.png';
+import acmw from '../../Assets/acm-w.svg';
+import { useSelector } from 'react-redux';
+import './Id.css';
 
 const Id = () => {
-  const {qrCodeData} = useSelector(state=>state)
+  const { qrCodeData, user } = useSelector(state => state);
+
   return (
     <section id="id" className="main-page">
       <div className="card-container">
@@ -12,18 +14,18 @@ const Id = () => {
           <img src={logo} alt="ACM BVP Logo" className="acm-logo" />
         </div>
         <div className="card-body">
-          <h1 className="name">HARSHIT BAREJA</h1>
-          <p className="role">WEB MASTER</p>
+          <h1 className="name">{user.name}</h1>
+          <p className="role">{user.position}</p>
         </div>
         <div className="card-qr">
           <img src={qrCodeData} alt="QR Code" className="qr-code" />
         </div>
         <div className="card-footer">
-          <img src="/path-to-your-logo/acm-w-logo.png" alt="ACM-W Logo" className="acm-w-logo" />
+          <img src={acmw} alt="ACM-W Logo" className="acm-w-logo" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Id
+export default Id;
